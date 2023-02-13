17d54e8dc4876ac0685095150b46a2e8f5b32abe
ademica <adema.sarajlija@gmail.com>
2023-02-13 01:27:14 +0100

    get by name and significance for attraction implemented

--- tourist-api/src/main/java/com/adema/touristapi/repo/AttractionRepository.java
+++ tourist-api/src/main/java/com/adema/touristapi/repo/AttractionRepository.java
@@ -1,12 +1,16 @@
-package com.adema.touristapi.attraction;
+package com.adema.touristapi.repo;
 
+import com.adema.touristapi.enumeration.Significance;
+import com.adema.touristapi.model.Attraction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AttractionRepository extends JpaRepository<Attraction, Long> {
 
     Optional<Attraction> findAttractionByName(String name);
+    List<Attraction> findAttractionsBySignificance(Significance significance);
 }
