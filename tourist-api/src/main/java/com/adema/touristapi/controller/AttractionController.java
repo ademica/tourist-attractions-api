c65331a49421da82d1101e02ad0d632457d0052c
ademica <adema.sarajlija@gmail.com>
2023-02-13 10:28:49 +0100

    added response status to attraction controller

--- tourist-api/src/main/java/com/adema/touristapi/controller/AttractionController.java
+++ tourist-api/src/main/java/com/adema/touristapi/controller/AttractionController.java
@@ -2,11 +2,19 @@ package com.adema.touristapi.controller;
 
 import com.adema.touristapi.enumeration.Significance;
 import com.adema.touristapi.model.Attraction;
+import com.adema.touristapi.model.Response;
 import com.adema.touristapi.service.AttractionService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("api/attractions")
@@ -19,29 +27,80 @@ public class AttractionController {
     }
 
     @GetMapping
-    public List<Attraction> getAttractions(){
-        return attractionService.getAttractions();
+    public ResponseEntity<Response> getAllAttractions(){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("attractions", attractionService.getAttractions()))
+                        .message("List of attractions")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
     }
 
     @GetMapping(path = "/name")
     public Attraction getAttractionByName(String name) {return  attractionService.getAttractionByName(name);}
 
-    @GetMapping("significance")
+    @GetMapping("/significance")
     public List<Attraction> getAttractionsBySignificance(Significance significance){
         return  attractionService.getAttractionsBySiginificance(significance);
     }
 
-    @PostMapping
-    public void registerNewAttraction(@RequestBody Attraction attraction){
-        attractionService.addAttraction(attraction);
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getAttraction(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("attraction", attractionService.getById(id)))
+                        .message("Attraction")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
     }
 
-    @DeleteMapping(path = "{attractionId}")
-    public void deleteAttraction(@PathVariable("attractionId") Long attractionId){
-        attractionService.delete(attractionId);
+    @PostMapping
+    public ResponseEntity<Response> saveAttraction(@RequestBody @Valid Attraction attraction) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("attraction", attractionService.addAttraction(attraction)))
+                        .message("Attraction created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Response> deleteAttraction(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("deleted", attractionService.delete(id)))
+                        .message("Attraction succesfully deleted")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
     }
 
     //uraditi aktivaciju znamenitosti
+    //update znamenitosti
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Response> updateAttraction(@PathVariable("id") Long id, @RequestBody Attraction attraction) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("updated", attractionService.update(id,attraction)))
+                        .message("Attraction updated")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
 
 
 
